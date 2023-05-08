882bc856c76878640a42a52aad26aac21e02bba9
Phil Deschaine <deschai5@gmail.com>
2023-05-08 07:49:24 -0700

    initial commit

---
+++ src/lib/components/bubbles/Bubbles.svelte
@@ -0,0 +1,14 @@
+<script>
+ 	import Bubble from './Bubble.svelte'
+	import { random } from '../../utils/random.js';
+
+ </script>
+
+ {#each Array(10) as _, i}
+	 <Bubble
+		--scale={random(2, 3.5) / 10}
+		--offset="{i * random(8, 10)}%"
+		--delay="{random(1000, 5000)}ms"
+		--speed="{random(10, 20)}s"
+	 />
+ {/each}
